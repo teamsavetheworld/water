c2450f6e796e3163974c332f46d4847d160a974e
Mikk <themikk@users.noreply.github.com>
2017-03-19 23:46:44 +0200

    geo_analysis.py
    Geospatial analysis for suitablility of rainwater greenhouse.

---
+++ geo_analysis.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+import netCDF4
+import pandas as pd
+import xarray as xr
+
+# Temperature & rainfall data from https://www.esrl.noaa.gov/psd/data/gridded/data.UDel_AirT_Precip.html
+
+# Load in temperature and rainfall data
+air_data_file = xr.open_dataset('/Users/Mikk/Documents/SaveTheWorld/Water/air.mon.mean.v401.nc',
+                                 decode_times=True)
+rain_data_file = xr.open_dataset('/Users/Mikk/Documents/SaveTheWorld/Water/precip.mon.total.v401.nc',
+                                 decode_times=True)
+df_t = air_data_file.to_dataframe()
+df_r = rain_data_file.to_dataframe()
+
+# Reformat data into nice columns
+df_t.reset_index(inplace=True)
+df_r.reset_index(inplace=True)
+
+# Define monthly timespan
+start_date = pd.tslib.Timestamp('2014-01-01 00:00:00')
+end_date = pd.tslib.Timestamp('2014-12-01 00:00:00')
+
+# Get mean monthly temperatures for only 2014
+df_t = df_t[(df_t.time >= start_date) & (df_t.time <= end_date)]
+
+# Get mean monthly rainfalls for only 2014
+df_r = df_r[(df_r.time >= start_date) & (df_r.time <= end_date)]
+
+# TODO: combine dataframes and set some sort of conditions for filtering by humidity
